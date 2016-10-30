9945790f5d831df95aad7cf819fb8e420591ab9e
Mary Olive <mary.olive@me.com>
2016-10-30 16:22:11 -0400

    Update Lab_3

--- Lab_3/Lab_3/Program.cs
+++ Lab_3/Lab_3/Program.cs
@@ -14,27 +14,42 @@ namespace Lab_3
             Console.WriteLine("Learn you squares and cubes!");
             Console.WriteLine(" ");
 
-            //get input
-            Console.Write("Enter an integer:  ");
-            double input = Convert.ToInt32(Console.ReadLine());
-
-            ////square and cube input
-            //double square = (Math.Pow(input, 2));
-            //double cube = (Math.Pow(input, 3));
-
-            //display output with padding
-            Console.WriteLine(" ");
-            Console.WriteLine("Number".PadRight(15) + "Squared".PadRight(15) + "Cubed".PadRight(15));
-            Console.WriteLine("=======".PadRight(15) + "=======".PadRight(15) + "=======".PadRight(15));
-
-            //set up for loop
-            int i = 1;
-            for (i = 1; i <= input; i++)
+            string proceed = "y";
+            while (proceed == "y")
             {
-                double square = (Math.Pow(i, 2));
-                double cube = (Math.Pow(i, 3));
-                Console.WriteLine(i + "".PadRight(14) + square + "".PadRight(14) + cube + "".PadRight(14));
+                //get input
+                Console.Write("Enter an integer:  ");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                //display output with padding
+                Console.WriteLine(" ");
+                Console.WriteLine("Number".PadRight(15) + "Squared".PadRight(15) + "Cubed".PadRight(15));
+                Console.WriteLine("=======".PadRight(15) + "=======".PadRight(15) + "=======".PadRight(15));
+
+                //set up for loop
+                int i = 1;
+                for (i = 1; i <= input; i++)
+                {
+                    double square = (Math.Pow(i, 2));
+                    double cube = (Math.Pow(i, 3));
+                    Console.WriteLine(i + "".PadRight(14) + square + "".PadRight(14) + cube + "".PadRight(14));
+                }
+                //Proceed?
+                Console.WriteLine("");
+                Console.Write("Continue? (y/n)");
+                proceed = (Console.ReadLine().ToLower());
+                if (proceed == "yes" || proceed == "y")
+                {
+                    proceed = "y";
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                }
             }
         }
+
     }
+
+
 }
