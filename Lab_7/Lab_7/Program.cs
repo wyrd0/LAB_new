dc665a2d405c5014493b36068b95942d5e3f2fbe
Mary Olive <mary.olive@me.com>
2016-11-03 09:46:02 -0400

    Add Histogram

---
+++ Lab_7/Lab_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to our C# class.  Which student would you like to learn more about? (Enter a number 1-11.)   ");
+            
+        }
+    }
+}
