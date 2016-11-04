203a32d540b61b41a0ecc41e52fe387798394ded
Mary Olive <mary.olive@me.com>
2016-11-04 14:05:20 -0400

    Create CircleApp class

---
+++ Lab_9-Circle/Lab_9-Circle/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_Circle
+{
+    class CircleApp
+    {
+        private double Radius;
+
+        public CircleApp(double radius)
+        {
+            Radius = radius;
+        }
+        public CircleApp()
+        {
+            
+        }
+        public double GetCircumference(double r)
+        {
+            double Circumference = 2 * Math.PI * Radius;
+            return Circumference;
+        }
+        public double GetArea(double r)
+        {
+            double Area = Math.Pow(Radius, 2)* Math.PI;
+            return Area;
+        }
+        private string FormatNumber(double n)
+        {
+            double Format = Math.Round(n, 2);
+            string Formatted = Format.ToString();
+            return Formatted;
+        }
+        public string GetFormattedCircumference(double n)
+        {
+            double cir = GetCircumference(Radius);
+            string FormattedCircumference = FormatNumber(cir);
+            return FormattedCircumference;
+        }
+        public string GetFormattedArea(double n)
+        {
+            double area = GetArea(Radius);
+            string FormattedArea = FormatNumber(area);
+            return FormattedArea;
+        }
+    }
+}
+
---
+++ Lab_9-Circle/Lab_9-Circle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+
+            //Prompt for radius
+            Console.Write("Please enter a radius:   ");
+            IsDouble(Console.ReadLine(), out radius);
+            
+
+        }
+        static bool IsDouble(string input, out double radius)
+        {
+            bool valid = double.TryParse(Console.ReadLine(), out radius);
+            return valid;
+        }
+    }
+}
