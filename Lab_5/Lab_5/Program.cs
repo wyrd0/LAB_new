b516940f58cc720cb8c939ba2d5054258a525aeb
Mary Olive <mary.olive@me.com>
2016-11-05 09:39:28 -0400

    Improve continue loop

--- Lab_5/Lab_5/Program.cs
+++ Lab_5/Lab_5/Program.cs
@@ -10,33 +10,87 @@ namespace Lab_5
     {
         static void Main(string[] args)
         {
+
             Console.Write("\nWelcome to the Grand Circus Casino!  Roll the dice? (y/n):   ");
             string proceed = Program.Proceed(Console.ReadLine());
-            Console.Write("\nHow many sides should the die have?   ");
-            int sides = Convert.ToInt32(Console.ReadLine());
+            while (proceed == "yes")
+            {
+                Console.Write("\nHow many sides should each die have?   ");
+                int sides = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Rolling 2 " + sides + "-sided dice...");
 
-            Console.Write ( &#9856);
+                Random r = new Random();
+                int die1 = r.Next(1, sides + 1);
+                int die2 = r.Next(1, sides + 1);
+
+                Console.WriteLine("You rolled a " + die1 + " and a " + die2 + ".");
+                Console.Write("Would you like to roll again? (y/n)  ");
+                proceed = Proceed(Console.ReadLine());
+
+            }
+
+
         }
-        //static int roll2Dice(int sides)
+        //private static string Proceed(string input)
         //{
-            
-
+        //    string proceed = (input.ToLower().Trim());
+        //    if (proceed == "y" || proceed == "yes" || proceed == "sure" || proceed == "ok" || proceed == "eys")
+        //    {
+        //        proceed = "yes";
+        //    }
+        //    else if (proceed == "no" || proceed == "n")
+        //    {
+        //        Console.WriteLine("");
+        //        Console.WriteLine("Goodbye! Thanks for playing!");
+        //        Console.WriteLine("");
+        //    }
+        //    else {
+        //        Console.WriteLine("Your response was not understood.  Please respond with 'y' or 'n'.");
+        //        Console.WriteLine("Would you like to roll again? (y/n):  ");
+        //        proceed = Console.ReadLine();
+        //    }
+        //    return proceed;
         //}
-        private static string Proceed(string v)
+        private static string Proceed(string input)
         {
-            string proceed = (v.ToLower().Trim());
-            if (proceed == "y" || proceed == "yes" || proceed == "sure" || proceed == "ok")
+            string proceed = (input.ToLower().Trim());
+            switch (proceed)
             {
-                proceed = "yes";
+                case "y":
+                    proceed = "yes";
+                    break;
+                case "eys":
+                    proceed = "yes";
+                    break;
+                case "yes":
+                    proceed = "yes";
+                    break;
+                case "ok":
+                    proceed = "yes";
+                    break;
+                case "sure":
+                    proceed = "yes";
+                    break;
+                case "no":
+                    proceed = "no";
+                    break;
+                case "n":
+                    proceed = "no";
+                    break;
+                default:
+                    Console.Write("Your response was not understood.  Please respond with 'y' or 'n'.  \nWould you like to roll the dice?   ");
+                    input = Console.ReadLine().ToLower().Trim();
+                    break;
             }
-            else
+            if (proceed == "no")
             {
-                Console.WriteLine("");
-                Console.WriteLine("Goodbye!");
-                Console.WriteLine("");
+                Console.WriteLine("\nGoodbye! Thanks for playing!\n");
             }
+            //else if (proceed == "yes")
+            //{
+            //    goto  case "y":;
+            //}
             return proceed;
         }
-        }
-
+    }
 }
---
+++ Lab_5/Lab_5/Random.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class RollOneRandom
+    {
+
+        private int NumberOfSides;
+        private int Die;
+
+        public RollOneRandom(int sides)     //constructor
+        {
+            sides = NumberOfSides;
+        }
+        #region ENCAPSULATED (get/set)
+        public int NumberOfSides1       //Encapsulated get/set
+        {
+            get
+            {
+                return NumberOfSides;
+            }
+
+            set
+            {
+                NumberOfSides = value;
+            }
+        }
+
+        public int Die1
+        {
+            get
+            {
+                return Die1;
+            }
+
+            set
+            {
+                Die1 = value;
+            }
+        }
+        #endregion
+
+        //public int NextTrial(int sides)         // method to roll 1 random
+        //{
+        //    Random r = new Random();
+        //    sides = NumberOfSides1;
+        //    int roll = r.Next(1, sides + 1);
+        //    return roll;         
+
+        //}
+        private int Roll(int sides)
+        {
+            Random r = new Random();
+            int Die = r.Next(1, sides + 1);
+            return Die;
+        }
+
+        public static implicit operator Random(RollOneRandom v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
