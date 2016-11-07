1eb0562fe202743c5ac18e8f74f3f6238ca6513b
Mary Olive <mary.olive@me.com>
2016-11-07 14:15:37 -0500

    Add Lab10

---
+++ Lab_10/Lab_10/Movie.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Movie
+    {
+        private string Title;
+        private string Category;
+
+        #region get/set
+        public string Title1
+        {
+            get
+            {
+                return Title;
+            }
+
+            set
+            {
+                Title = value;
+            }
+        }
+
+        public string Category1
+        {
+            get
+            {
+                return Category;
+            }
+
+            set
+            {
+                Category = value;
+            }
+            #endregion
+
+            //constructors
+           public Movie(string title, string category)
+        {
+           Title1 = title;
+           Category1 = Category;
+        }
+            public Movie(string category)
+        {
+            Category1 = category;
+        }
+        public MatchCategory(string category)
+        {
+
+        }
+        
+    }
+}
---
+++ Lab_10/Lab_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> MovieList = new List<Movie>();
+            string movie, category;
+            Console.WriteLine("Welcome to MovieList!\n");
+            Console.WriteLine("There are 10 movies in this list. \nWhat category are you interested in?    ");
+            category = Console.ReadLine();
+
+
+        }
+    }
+}
